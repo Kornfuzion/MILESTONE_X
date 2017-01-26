b501a6778f4bcb7a93e8cddcd198f4d387d00acf
James <jamess.shen@mail.utoronto.ca>
2017-01-26 16:49:36 -0500

    added message protocol and serialization/deserialization

--- echoClient/src/client/ClientSocketListener.java
+++ echoClient/src/client/ClientSocketListener.java
@@ -4,7 +4,7 @@ public interface ClientSocketListener {
 
 	public enum SocketStatus{CONNECTED, DISCONNECTED, CONNECTION_LOST};
 	
-	public void handleNewMessage(TextMessage msg);
+	public void handleNewMessage(Message msg);
 	
 	public void handleStatus(SocketStatus status);
 }
