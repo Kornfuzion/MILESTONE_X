50ad0634cf267a6793488442afa954b422e7d3da
victor ko <victor.ko@mail.utoronto.ca>
2017-01-27 16:22:50 -0500

    adding clientTest and MessageTest

---
+++ BasicStorageServer-stub/src/testing/ClientTest.java
@@ -0,0 +1,27 @@
+package testing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.ClassLoader;
+
+import junit.framework.TestCase;
+
+
+public class ClientTest extends TestCase {
+	@Before
+    public void setUp() {
+	//put here
+	}
+	
+	@After
+    public void tearDown() {
+		//empty
+    }
+	
+	@Test
+	public void testOne(){
+		//empty	
+	}
+}
